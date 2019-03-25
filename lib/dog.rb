57a2dd69f771a1d52aace26f8e65ca3aac92921a
Karen V <karenvo@usc.edu>
2019-03-25 06:38:18 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,9 +4,12 @@ class Dog
     @name = name
     @@all << self
   end
-
+  def all
+    #iterates over all of the individual dogs stored in @@all array and 'puts' out their name to terminal
+    
+  end
   def clear_all
-
+  
   end
 
 end
