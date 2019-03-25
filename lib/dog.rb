f3b21317d06e4fd7ec46865873a0aec962061f0e
Karen V <karenvo@usc.edu>
2019-03-25 06:43:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ class Dog
   end
   def all
     #iterates over all of the individual dogs stored in @@all array and 'puts' out their name to terminal
-    @@all.each do |name|
+    @@all.each do |dog|
       puts dog.name
     end
   end
